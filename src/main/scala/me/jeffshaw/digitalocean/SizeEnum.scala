c0f99b7b3ca7138723899b6efa049c3ebcc86d8d
Jeff Shaw <shawjef3@gmail.com>
2017-09-11 22:51:55 -0400

    add new droplet sizes

--- src/main/scala/me/jeffshaw/digitalocean/SizeEnum.scala
+++ src/main/scala/me/jeffshaw/digitalocean/SizeEnum.scala
@@ -64,6 +64,26 @@ case object `m-224gb` extends SizeEnum {
   override val slug: String = "m-224gb"
 }
 
+case object `c-2` extends SizeEnum {
+  override val slug = "c-2"
+}
+
+case object `c-4` extends SizeEnum {
+  override val slug = "c-4"
+}
+
+case object `c-8` extends SizeEnum {
+  override val slug = "c-8"
+}
+
+case object `c-16` extends SizeEnum {
+  override val slug = "c-16"
+}
+
+case object `c-32` extends SizeEnum {
+  override val slug = "c-32"
+}
+
 case class OtherSize(slug: String) extends SizeEnum
 
 object SizeEnum {
@@ -89,6 +109,11 @@ object SizeEnum {
     `64gb`.slug -> `64gb`,
     `m-64gb`.slug -> `m-64gb`,
     `m-128gb`.slug -> `m-128gb`,
-    `m-224gb`.slug -> `m-224gb`
+    `m-224gb`.slug -> `m-224gb`,
+    `c-2`.slug -> `c-2`,
+    `c-4`.slug -> `c-4`,
+    `c-8`.slug -> `c-8`,
+    `c-16`.slug -> `c-16`,
+    `c-32`.slug -> `c-32`
   )
 }
