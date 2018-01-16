eae42106d4b9d46e4b862324a6edc1d997353994
Jeff Shaw <shawjef3@gmail.com>
2018-01-15 23:09:50 -0500

    more useful DigitalOcean class

--- src/main/scala/me/jeffshaw/digitalocean/DigitalOcean.scala
+++ src/main/scala/me/jeffshaw/digitalocean/DigitalOcean.scala
@@ -2,22 +2,48 @@ package me.jeffshaw.digitalocean
 
 import scala.concurrent._
 
+/**
+  * Provides a snapshot of your Digital Ocean assets.
+  */
 case class DigitalOcean(
-  droplets: Iterator[Droplet],
-  images: Iterator[Image],
-  regions: Iterator[Region],
-  sizes: Iterator[Size]
+  domains: Set[dns.Domain],
+  droplets: Set[Droplet],
+  firewalls: Set[Firewall],
+  floatingIps: Set[FloatingIp],
+  images: Set[Image],
+  regions: Set[Region],
+  sizes: Set[Size],
+  sshKeys: Set[SshKey],
+  tags: Set[Tag],
+  volumes: Set[Volume]
 )
 
 object DigitalOcean {
-  def list(implicit client: DigitalOceanClient, ec: ExecutionContext): Future[DigitalOcean] = {
+  def list()(implicit client: DigitalOceanClient, ec: ExecutionContext): Future[DigitalOcean] = {
     for {
+      domains <- dns.Domain.list()
       droplets <- Droplet.list()
+      firewalls <- Firewall.list()
+      floatingIps <- FloatingIp.list()
       images <- Image.list()
       regions <- Region.list()
       sizes <- Size.list()
+      sshKeys <- SshKey.list()
+      tags <- Tag.list()
+      volumes <- Volume.list()
     } yield {
-      DigitalOcean(droplets, images, regions, sizes)
+      DigitalOcean(
+        domains = domains.toSet,
+        droplets = droplets.toSet,
+        firewalls = firewalls.toSet,
+        floatingIps = floatingIps.toSet,
+        images = images.toSet,
+        regions = regions.toSet,
+        sizes = sizes.toSet,
+        sshKeys = sshKeys.toSet,
+        tags = tags.toSet,
+        volumes = volumes.toSet
+      )
     }
   }
 }
